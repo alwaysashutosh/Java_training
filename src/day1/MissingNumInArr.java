ad738f3eb9865696197f52f4218738c373c9cb17
Rahul <mouryarahul543@gmail.com>
2026-01-10 23:04:07 +0530

    missing num in array

--- src/day1/MissingNumInArr.java
+++ src/day1/MissingNumInArr.java
@@ -2,8 +2,12 @@ package day1;
 
 public class MissingNumInArr {
     public static void main(String[] args) {
-        int [] arr={1,2,3,4,5,7,8,9,10};
+        int [] arr={1,2,3,4,5,7,8,9,10,11};
         int ans=0;
+        for (int i = 1; i <=arr.length+1; i++) {
+            ans ^= i;
+        }
+
         for(int i=0; i<arr.length; i++){
             ans^=arr[i];
         }
