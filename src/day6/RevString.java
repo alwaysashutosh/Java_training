a189c844169e6ade7e3f7e397309f0223ca2df07
Rahul <mouryarahul543@gmail.com>
2026-01-17 10:24:11 +0530

    RevString prob

---
+++ src/day6/RevString.java
@@ -0,0 +1,8 @@
+package day6;
+
+public class RevString {
+    public static void main(String[] args) {
+        StringBuilder name = new StringBuilder("Rahul");
+        System.out.println(name.reverse());
+    }
+}
